68d787e2ba219749f724875a49e2dbd6672c6a5b
sven-berger <bergersvenoliver@gmail.com>
2024-08-07 18:59:25 +0200

    Verschönerung - PHP-Version

--- php/includes/var.inc.php
+++ php/includes/var.inc.php
@@ -3,3 +3,6 @@
 $sidebarLeft = file_get_contents("https://codevoyage.de/python/templates/sidebarLeft.html");
 $sidebarRight = file_get_contents("https://codevoyage.de/python/templates/sidebarRight.html");
 $footer = file_get_contents("https://codevoyage.de/python/templates/footer.html");
+
+$phpVersion = phpversion();
+$phpVersion = explode('-', $phpVersion)[0];
