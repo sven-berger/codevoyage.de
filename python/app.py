772055a06aa5d299de8bffd96a73de000b2d64c3
sven-berger <bergersvenoliver@gmail.com>
2024-08-07 19:57:59 +0200

    Waiting....

--- python/app.py
+++ python/app.py
@@ -1,5 +1,21 @@
+import sys
 from flask import Flask, render_template
+import flask
 app = Flask(__name__)
+
+@app.context_processor
+def inject_versions():
+    return dict(
+        python_version=get_python_version(),
+        flask_version=get_flask_version()
+    )
+
+def get_python_version():
+    version_info = sys.version_info
+    return f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+
+def get_flask_version():
+    return flask.__version__
 
 @app.route('/')
 def hallo():
