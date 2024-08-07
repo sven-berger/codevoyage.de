d49967db26cb4af1c34f784b23b04c60bde082bd
sven-berger <bergersvenoliver@gmail.com>
2024-08-07 08:49:48 +0200

    Added flex.sidebar again

--- acp/includes/footer.inc.php
+++ acp/includes/footer.inc.php
@@ -5,6 +5,7 @@
 
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/sidebarLeft.inc.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/acp/includes/sidebarRight.inc.php");
 ?>
 
 </div>
