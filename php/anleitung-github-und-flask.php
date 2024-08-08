feda9e7894bcd9daf9282930f263536bd25bbcee
sven-berger <bergersvenoliver@gmail.com>
2024-08-08 22:06:29 +0200

    Codeanpassung

--- php/anleitung-github-und-flask.php
+++ php/anleitung-github-und-flask.php
@@ -35,7 +35,7 @@
 <section class="section">
 <div class="sectionContent">
 <p style="font-weight: 400;">sudo apt install python3 python3-pip python3-venv libapache2-mod-wsgi-py3</p>
-<p><small><i>Dies gilt für Ubuntu. Die Warhrscheinlichkeit, dass Debian mehr Pakete benötigt, ist gegeben.<i></small></p>
+<p class="notice"></p>
 <p><br/></p>
 <p>cd <span style="font-weight: bold; color:darkred;">/verzeichnis-in-dem-flask-ausgeführt werden soll/</span></p>
 <p><br/></p>
