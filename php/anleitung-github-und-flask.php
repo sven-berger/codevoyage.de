a92f255d6194719a014d20fedd1e219690ada900
sven-berger <bergersvenoliver@gmail.com>
2024-08-09 07:13:34 +0200

    Erweiterung der Anleitung

--- php/anleitung-github-und-flask.php
+++ php/anleitung-github-und-flask.php
@@ -37,7 +37,7 @@
 <p>sudo apt install python3 python3-pip python3-venv libapache2-mod-wsgi-py3</p>
 <p class="notice">Dies gilt für Ubuntu. Die Wahrscheinlichkeit, dass Debian mehr Pakete benötigt, ist gegeben</p>
 <p><br/></p>
-<p>cd <span style="font-weight: bold; color:darkred;">/verzeichnis-in-dem-flask-ausgeführt werden soll/</span></p>
+<p>cd <span style="font-weight: bold; color:darkred;">/verzeichnis-in-dem-python/flask-ausgeführt werden soll/</span></p>
 <p><br/></p>
 <p>python3 -m venv venv</p>
 <p><br/></p>
@@ -48,7 +48,48 @@
 <p>pip install flask-restful</p>
 <p><br/></p>
 <p>deactivate</p>
-
+</div>
+</section>
+<h3 class="boxTitle">FlaskApp konfigurieren</h3>
+<section class="section">
+<div class="sectionContent">
+<p>nano flaskapp.wsgi</p>
+<div class="codeContent">
+<p>import sys</p>
+<p>import logging</p>
+<p><br/></p>
+<p>sys.path.insert(0, '<span style="font-weight: bold; color:darkred;">/verzeichnis-in-dem-python/flask-ausgeführt werden soll/</span>')</p>
+<p>from app import app as application</p>
+<p>logging.basicConfig(stream=sys.stderr)</p>
+</div>
+</section>
+<h3 class="boxTitle">App anlegen</h3>
+<section class="section">
+<div class="sectionContent">
+<p>nano app.py</p>
+<div class="codeContent">
+<p>from flask import Flask</p>
+<p><br/></p>
+<p>app = Flask(__name__)</p>
+<p><br/></p>
+<p>@app.route('/')</p>
+<p>def hello():</p>
+<p>    return "Hello, World!"</p>
+<p><br/></p>
+<p>if __name__ == '__main__':</p>
+<p>    app.run(debug=True)</p>
+</div>
+</section>
+<h3 class="boxTitle">Apache-Konfiguration <span style="font-weight: bold;">anpassen</span></h3>
+<section class="section">
+<div class="sectionContent">
+<p>WSGIDaemonProcess flaskapp threads=5</p>
+<p>WSGIScriptAlias / <span style="font-weight: bold; color:darkred;">/verzeichnis-der-python/flask-instanz/</span>flaskapp.wsgi</p>
+<p><br/></p>
+<p><Directory /var/customers/webs/codevoyage/python></p>
+<p>        Require all granted</p>
+<p></Directory></p>
+</div>
 <?php
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/includes/footer.inc.php");
 ?>
