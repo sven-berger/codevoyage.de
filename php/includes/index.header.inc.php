f1d4d0e960ecca0d729bf93d5fc92f37a1e99f82
sven-berger <bergersvenoliver@gmail.com>
2024-08-09 09:46:17 +0200

    Add Highlighter

--- php/includes/index.header.inc.php
+++ php/includes/index.header.inc.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://codevoyage.de/python/static/css/style.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.0/highlight.min.js"></script>
+    <script>hljs.highlightAll();</script>
     <title><?php echo $pageTitle; ?></title>
 </head>
 
